e2e7befe212b1340f851af88f3de4133dc33df14
Navan Chauhan <navanchauhan@gmail.com>
2024-10-13 17:49:09 -0600

    Initial Commit

---
+++ iTexSnip/iTexSnipApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTexSnipApp.swift
+//  iTexSnip
+//
+//  Created by Navan Chauhan on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct iTexSnipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
